87a2d3c0c830ef1de0777e91cc72efc215ce6425
Christian PAZMANY <PazmanyC@ad.fms-ea.com>
2023-07-05 09:53:06 +0200

    Navbar and Footer added.

--- AngularApiRestTraining/src/app/app-routing.module.ts
+++ AngularApiRestTraining/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrainingsComponent } from './component/trainings/trainings.component';
 import { UpdeteTrainingComponent } from './component/updete-training/updete-training.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'trainings', component: TrainingsComponent },
   { path: 'updete-training', component: UpdeteTrainingComponent },
 ];
